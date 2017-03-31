baee0634b2e349563744cf25ef64588786f4b94c
haner <haner.du@qq.com>
2017-03-31 11:47:43 +0800

    [login page commit] [sass helper util]

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -1,48 +1,50 @@
 <template>
-  <div class="row">
-    <form class="col-lg-4 col-lg-offset-4" @submit.prevent="login">
-      <div class="alert alert-danger" role="alert" v-if="errorMsg" v-text="errorMsg"></div>
-      <div class="panel">
-        <div class="panel-heading">
-          <h3 class="panel-title">电子优惠券系统</h3>
+  <div class="row login-container">
+    <div class="col-lg-6">
+      <img class="login-left-img" src="../assets/image/img-login-left@2X.png" alt="">
+    </div>
+    <div class="col-lg-6 login-right-form">
+      <form @submit.prevent="login">
+        <div class="alert alert-danger" role="alert" v-if="errorMsg" v-text="errorMsg"></div>
+        <div class="form-group">
+          <img class="logo" src="../assets/image/logo.png" alt="">
+          <span class="system-name">停车王电子优惠券系统</span>
         </div>
-        <div class="panel-body">
-          <div class="form-group">
-            <div class="input-group">
-              <div class="input-group-btn">
-                <input type="hidden" name="role" value="2"/>
-                <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown">
-                  <span v-text="currentRole.role"></span>
-                  <span class="caret"></span>
-                </button>
-                <ul class="dropdown-menu">
-                  <li v-for="item in roles" @click="selectRole(item)">
-                    <a v-text="item.role" :data-role="item.value"></a>
-                  </li>
-                </ul>
-              </div>
-              <input type="text" class="form-control" name="username" v-model="loginData.username" placeholder="账号">
+        <div class="form-group">
+          <div class="input-group">
+            <div class="input-group-btn">
+              <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown">
+                <span v-text="currentRole.role"></span>
+                <span class="caret"></span>
+              </button>
+              <ul class="dropdown-menu">
+                <li v-for="item in roles" @click="selectRole(item)">
+                  <a v-text="item.role" :data-role="item.value"></a>
+                </li>
+              </ul>
             </div>
-          </div>
-          <div class="form-group" v-show="currentRole.value != 2">
-            <input type="text" class="form-control" name="mallid" v-model="loginData.mallid" placeholder="商场编号">
-          </div>
-          <div class="form-group">
-            <input type="password" class="form-control" name="password" v-model="loginData.password" placeholder="密码">
-          </div>
-          <div class="form-group">
-            <button class="btn btn-primary btn-block" :disabled="isLogin" type="submit"
-                    v-text="isLogin ? '登录中...':'登 录'"></button>
+            <input type="text" class="form-control" name="username" v-model="loginData.username" placeholder="账号">
           </div>
         </div>
-      </div>
-    </form>
+        <div class="form-group" v-show="currentRole.value !== 2">
+          <input type="text" class="form-control" name="mallid" v-model="loginData.mallid" placeholder="商场编号">
+        </div>
+        <div class="form-group">
+          <input type="password" class="form-control" name="password" v-model="loginData.password" placeholder="密码">
+        </div>
+        <div class="form-group">
+          <button class="btn btn-primary btn-block" :disabled="isLogin" type="submit"
+                  v-text="isLogin ? '登录中...':'登 录'"></button>
+        </div>
+      </form>
+    </div>
   </div>
 </template>
 <script>
   import * as UserService from '../api/user';
   import Storage from '../commons/utils/Storage';
   import layer from 'layui-layer';
+  import '../assets/style/login.scss';
 
   export default {
     created(){
